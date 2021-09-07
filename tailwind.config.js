e2351758bb9bae795b0d177134015a774f49e1ab
szpital07 <szpitalzts@outlook.com>
2021-09-07 09:04:13 +0200

    breif

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  important: true,
+  theme: {
+    extend: {
+      colors: {
+        'cyan':'#2AB2AF',
+        'brightyellow':'#C0DF34',
+        'lightgray':'#E5EFF5',
+        'grayishblue':'#98A6BD',
+        'cyangreen':'#3DB5AA',
+      },
+      width: {
+        '1440': '1440px',
+       }
+  },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
